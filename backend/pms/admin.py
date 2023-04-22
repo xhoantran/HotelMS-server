7e851925aed4a91823c25a954f06febdbf54efc4
Hoan Tran <xhoantran@gmail.com>
2023-04-22 19:41:49 -0400

    add tz Vietnam

--- backend/pms/admin.py
+++ backend/pms/admin.py
@@ -1,11 +1,5 @@
 from django.apps import apps
 from django.contrib import admin
-from django_celery_beat.models import (
-    CrontabSchedule,
-    IntervalSchedule,
-    PeriodicTask,
-    PeriodicTasks,
-)
 
 from backend.rms.models import AvailabilityBasedTriggerRule, DynamicPricingSetting
 
@@ -15,15 +9,19 @@ for model in apps.get_models():
     if model.__name__ and admin.site.is_registered(model):
         admin.site.unregister(model)
 
+
+# PMS
 admin.site.register(HotelGroup)
 admin.site.register(Hotel)
 admin.site.register(RoomType)
 admin.site.register(RatePlan)
 
+# RMS
 admin.site.register(DynamicPricingSetting)
 admin.site.register(AvailabilityBasedTriggerRule)
 
-admin.site.register(PeriodicTask)
-admin.site.register(PeriodicTasks)
-admin.site.register(IntervalSchedule)
-admin.site.register(CrontabSchedule)
+
+# admin.site.register(PeriodicTask)
+# admin.site.register(PeriodicTasks)
+# admin.site.register(IntervalSchedule)
+# admin.site.register(CrontabSchedule)

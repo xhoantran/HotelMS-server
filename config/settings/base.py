40b8ffc964f5714c01098ad568c0883595ced2b8
Hoan Tran <xhoantran@gmail.com>
2023-04-26 15:30:57 -0400

    clean config

--- config/settings/base.py
+++ config/settings/base.py
@@ -236,10 +236,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [
-    ("Hoan Tran", "xhoantran@gmail.com"),
-    ("Minh Tran", "minh.tranbahoang@gmail.com"),
-]
+ADMINS = [("Hoan Tran", "xhoantran@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -115,8 +115,6 @@ EMAIL_SUBJECT_PREFIX = env(
 
 # ADMIN
 # ------------------------------------------------------------------------------
-# Admins list
-ADMINS = [("Hoan Tran", "xhoantran@gmail.com")]
 # Django Admin URL regex.
 ADMIN_URL = env("DJANGO_ADMIN_URL")
 

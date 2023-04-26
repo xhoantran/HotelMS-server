5623a58ef26a27c53537979386e18aea97e424cc
Hoan Tran <xhoantran@gmail.com>
2023-04-26 14:59:36 -0400

    fix anymail

--- config/settings/production.py
+++ config/settings/production.py
@@ -127,7 +127,7 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
-EMAIL_BACKEND = "anymail.backends.amazon_ses2.EmailBackend"
+EMAIL_BACKEND = "anymail.backends.amazon_sesv2.EmailBackend"
 ANYMAIL = {}
 
 # Collectfast

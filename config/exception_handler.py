c80106e78279ea1c18eb8b0f4f942d83c36c4e9c
Hoan Tran <xhoantran@gmail.com>
2023-05-01 22:05:43 -0400

    lint

--- config/exception_handler.py
+++ config/exception_handler.py
@@ -1,9 +1,9 @@
 from django.core import exceptions
 from django.views import View
 from rest_framework.response import Response as DRFResponse
+from rest_framework.settings import api_settings
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.views import exception_handler
-from rest_framework.settings import api_settings
 
 
 def custom_exception_handler(exc: Exception, context: View):

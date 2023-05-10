8434161899092d1d18d76e694a482c05d09ceaed
Hoan Tran <xhoantran@gmail.com>
2023-05-10 13:27:47 -0400

    lint

--- backend/rms/signals.py
+++ backend/rms/signals.py
@@ -1,6 +1,6 @@
 import json
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from django_celery_beat.models import CrontabSchedule, PeriodicTask

df4a0e0e24239a9e4c0746e0e36214c430513f5b
Hoan Tran <xhoantran@gmail.com>
2023-05-24 13:19:14 -0400

    Remove CONN_MAX_AGE - pgbouncer or aws rds proxy will handle this - fix too many idle db connection unclosed

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,10 +16,6 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.hanz.com.vn"])
 
-# DATABASES
-# ------------------------------------------------------------------------------
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
-
 # CACHES
 # ------------------------------------------------------------------------------
 CACHES = {

27509a0b547b9da126dec1135258e41ef90ff750
Hoan Tran <xhoantran@gmail.com>
2023-05-25 21:07:47 -0400

    update swagger test

--- backend/users/tests/test_swagger.py
+++ backend/users/tests/test_swagger.py
@@ -1,22 +1,21 @@
+import pytest
 from django.urls import reverse
 
 
-def test_swagger_accessible_by_super_admin(super_admin, get_api_client):
+def test_swagger_accessible_by_admin(admin_client):
     url = reverse("api-docs")
-    super_admin_api_client = get_api_client(super_admin)
-    response = super_admin_api_client.get(url)
+    response = admin_client.get(url)
     assert response.status_code == 200
 
 
-def test_swagger_ui_not_accessible_by_normal_user(user, get_api_client):
+@pytest.mark.django_db
+def test_swagger_ui_not_accessible_by_normal_user(client):
     url = reverse("api-docs")
-    api_client = get_api_client(user)
-    response = api_client.get(url)
+    response = client.get(url)
     assert response.status_code == 403
 
 
-def test_api_schema_generated_successfully(super_admin, get_api_client):
+def test_api_schema_generated_successfully(admin_client):
     url = reverse("api-schema")
-    super_admin_api_client = get_api_client(super_admin)
-    response = super_admin_api_client.get(url)
+    response = admin_client.get(url)
     assert response.status_code == 200
